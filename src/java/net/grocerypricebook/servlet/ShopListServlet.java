b70ce94751dd215ca3f7e172f310fd04ea496a08
Mike Hayes <mkhayes@gmail.com>
2013-03-09 12:40:10 -0500

    Re-arranged packages.

--- src/java/net/grocerypricebook/servlet/ShopListServlet.java
+++ src/java/net/grocerypricebook/servlet/ShopListServlet.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.servlet;
+package net.grocerypricebook.servlet;
 
 import Users.Users;
 import java.io.IOException;
